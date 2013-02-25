08b79915a5d7ba5e7e4c9805d8e1df9cef109112
Ruediger Gad <r.c.g@gmx.de>
2013-02-25 09:17:05 +0100

    Update version info to 1.1.0.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-;(defproject clj-assorted-utils "1.0.0"
-(defproject clj-assorted-utils "1.1.0-SNAPSHOT"
+;(defproject clj-assorted-utils "1.1.0"
+(defproject clj-assorted-utils "1.2.0-SNAPSHOT"
   :description "Unsorted bunch of helper and utility functions."
   :dependencies [[org.clojure/clojure "1.4.0"]]
 )
