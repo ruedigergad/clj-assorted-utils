8660b7ba2d643f993a6615332c8d3b6fdb6c68f5
Ruediger Gad <r.c.g@gmx.de>
2013-03-09 15:30:04 +0100

    Add copyright to documentation.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :description "Unsorted bunch of helper and utility functions."
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :dev-dependencies [[lein-autodoc "0.9.0"]]
+  :autodoc {:copyright "Copyright 2012, 2013 Ruediger Gad --
+                        clj-assorted-utils is released under terms of the Eclipse Public License, the same as Clojure." }
 )
