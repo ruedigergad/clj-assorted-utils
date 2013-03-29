1e7d5900affc797748c8f86b0e719155bf93c60a
Ruediger Gad <r.c.g@gmx.de>
2013-03-29 16:49:58 +0100

    Update project documentation.

--- project.clj
+++ project.clj
@@ -5,4 +5,7 @@
   :dev-dependencies [[lein-autodoc "0.9.0"]]
   :autodoc {:copyright "Copyright 2012, 2013 Ruediger Gad --
                         clj-assorted-utils is released under terms of the Eclipse Public License, the same as Clojure." }
+  :html5-docs-docs-dir "autodoc"
+  :html5-docs-ns-includes #"^clj-assorted-utils.*"
+  :html5-docs-repository-url "https://github.com/ruedigergad/clj-assorted-utils/blob/master"
 )
