0a7ec54fcfe9b601b3e4f53d0333782e2a0cc8aa
Ruediger Gad <rgad@fb2.fh-frankfurt.de>
2013-12-03 10:52:02 +0100

    Add type hints.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
 ;(defproject clj-assorted-utils "1.5.0-SNAPSHOT"
   :description "Unsorted bunch of helper and utility functions."
   :dependencies [[org.clojure/clojure "1.5.1"]]
+  :global-vars {*warn-on-reflection* true}
   :html5-docs-docs-dir "ghpages/doc"
   :html5-docs-ns-includes #"^clj-assorted-utils.*"
   :html5-docs-repository-url "https://github.com/ruedigergad/clj-assorted-utils/blob/master"
