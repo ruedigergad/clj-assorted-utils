e0b5859f387596a30f3efd709480a291b2caedd5
Ruediger Gad <r.c.g@gmx.de>
2015-11-07 19:16:25 +0100

    Add missing 'is'.

--- test/clj_assorted_utils/test/util.clj
+++ test/clj_assorted_utils/test/util.clj
@@ -207,9 +207,8 @@
 (defn test-args-fn [a b c] (+ a b c))
 
 (deftest get-defn-arglists-test
-  (= '([a b c]) (get-defn-arglists test-args-fn)))
+  (is (= '([a b c]) (get-defn-arglists test-args-fn))))
 
-(deftest get-
 
 
 ;;;
