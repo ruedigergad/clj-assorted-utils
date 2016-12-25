4646d20d4152dcc0f941ef23c622e869e8a79412
Ruediger Gad <r.c.g@gmx.de>
2016-12-25 22:15:37 +0100

    Update version info.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-;(defproject clj-assorted-utils "1.15.0"
-(defproject clj-assorted-utils "1.16.0-SNAPSHOT"
+;(defproject clj-assorted-utils "1.16.0"
+(defproject clj-assorted-utils "1.17.0-SNAPSHOT"
   :description "Unsorted bunch of helper and utility functions."
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :global-vars {*warn-on-reflection* true}
