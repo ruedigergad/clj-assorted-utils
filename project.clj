6c54f8fc465d84c0d1c0043bd6c4db5a25d66953
Ruediger Gad <r.c.g@gmx.de>
2021-04-11 11:32:14 +0200

    Update version infor and dependencies.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-assorted-utils "1.18.7"
+(defproject clj-assorted-utils "1.18.8"
 ;(defproject clj-assorted-utils "1.19.0-SNAPSHOT"
   :description "Unsorted bunch of helper and utility functions."
   :dependencies [[org.clojure/clojure "1.10.3"]]
@@ -10,7 +10,7 @@
   :test2junit-run-ant true
   :aot [clj-assorted-utils.JavaUtils]
   :plugins [[lein-cloverage "1.0.6"]]
-  :profiles  {:repl  {:dependencies  [[jonase/eastwood "0.3.4" :exclusions  [org.clojure/clojure]]]}}
+  :profiles  {:repl  {:dependencies  [[jonase/eastwood "0.4.0" :exclusions  [org.clojure/clojure]]]}}
   :license {:name "Eclipse Public License (EPL) - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
