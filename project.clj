186424fcfec5da0344527b9421b25b6cbadb9dfb
Ruediger Gad <r.c.g@gmx.de>
2022-07-26 21:19:55 +0200

    Update version info.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clj-assorted-utils "1.18.8"
-;(defproject clj-assorted-utils "1.19.0-SNAPSHOT"
+(defproject clj-assorted-utils "1.19.0"
+;(defproject clj-assorted-utils "1.19.1-SNAPSHOT"
   :description "Unsorted bunch of helper and utility functions."
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :global-vars {*warn-on-reflection* true}
   :html5-docs-docs-dir "docs/doc"
   :html5-docs-ns-includes #"^clj-assorted-utils.*"
@@ -10,7 +10,7 @@
   :test2junit-run-ant true
   :aot [clj-assorted-utils.JavaUtils]
   :plugins [[lein-cloverage "1.0.9"] [test2junit "1.4.2"] [lein-html5-docs "3.0.3"]]
-  :profiles  {:repl  {:dependencies  [[jonase/eastwood "0.4.0" :exclusions  [org.clojure/clojure]]]}}
+  :profiles  {:repl  {:dependencies  [[jonase/eastwood "1.2.4" :exclusions  [org.clojure/clojure]]]}}
   :license {:name "Eclipse Public License (EPL) - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
